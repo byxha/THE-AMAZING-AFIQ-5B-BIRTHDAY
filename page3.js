5c4e5911c03f5392a274f34f09f9b444edba746c
byxha <azaninabiha@gmail.com>
2026-07-17 04:26:04 +0800

    Update page3.js

--- page3.js
+++ page3.js
@@ -1,1 +1,23 @@
+const title = document.getElementById("typing-title");
 
+const text = title.innerHTML;
+
+title.innerHTML = "";
+
+let i = 0;
+
+function typing() {
+
+    if (i < text.length) {
+
+        title.innerHTML += text.charAt(i);
+
+        i++;
+
+        setTimeout(typing, 80);
+
+    }
+
+}
+
+typing();
