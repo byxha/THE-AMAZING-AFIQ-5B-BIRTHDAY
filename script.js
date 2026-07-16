fec8e40f3ac67e3b498551c1841b30692fd39ffd
byxha <azaninabiha@gmail.com>
2026-07-17 00:13:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-alert("SCRIPT WORKING!");
+const countdownDate = new Date(Date.now() + 5000).getTime();
 
-let count = 5;
+const timer = setInterval(function () {
 
-let timer = setInterval(function () {
-    document.getElementById("seconds").innerHTML = count;
-    count--;
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
 
-    if (count < 0) {
+    if (distance <= 0) {
         clearInterval(timer);
+
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+
         document.getElementById("bebanSection").classList.remove("hidden");
+
+        return;
     }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
 
 }, 1000);
