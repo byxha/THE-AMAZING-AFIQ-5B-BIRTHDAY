2c253164855b5a4b332fcf3bb5ee827d8a432407
byxha <azaninabiha@gmail.com>
2026-07-17 00:01:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+// TEST COUNTDOWN: 5 seconds
+const countdownDate = new Date(Date.now() + 5000).getTime();
 
+const timer = setInterval(function () {
+
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+
+    if (distance <= 0) {
+
+        clearInterval(timer);
+
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+
+
+        // Show ARE YOU BEBAN section
+        document.getElementById("bebanSection").classList.remove("hidden");
+
+        return;
+    }
+
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+
+}, 1000);
