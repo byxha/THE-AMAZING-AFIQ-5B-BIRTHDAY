ba8460bf5413a57477aff4c68c36a4aa2764723c
byxha <azaninabiha@gmail.com>
2026-07-17 00:16:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,44 @@ const timer = setInterval(function () {
     document.getElementById("seconds").innerHTML = seconds;
 
 }, 1000);
+const noBtn = document.getElementById("noBtn");
+
+let escapeCount = 0;
+
+function moveNoButton() {
+
+    escapeCount++;
+
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+
+    // lari jauh bertentangan (random jauh)
+    let newX = Math.random() * (screenWidth - 150);
+    let newY = Math.random() * (screenHeight - 80);
+
+    noBtn.style.position = "fixed";
+    noBtn.style.left = newX + "px";
+    noBtn.style.top = newY + "px";
+
+    // selepas 3 kali, dia lari keluar
+    if (escapeCount >= 3) {
+
+        noBtn.innerHTML = "🏃 NO 😭";
+
+        noBtn.style.transition = "2s ease";
+
+        setTimeout(() => {
+            noBtn.style.left = "120vw";
+            noBtn.style.top = "50vh";
+        }, 100);
+
+        setTimeout(() => {
+            noBtn.remove();
+        }, 2200);
+    }
+}
+
+
+// Untuk phone + laptop
+noBtn.addEventListener("mouseover", moveNoButton);
+noBtn.addEventListener("touchstart", moveNoButton);
